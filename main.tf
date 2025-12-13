68d927d116b2957f2683e883f6c329020d3fb9a0
RamCupido <ramses-02@hotmail.es>
2025-12-12 20:15:31 -0500

    fix: launch template version for docker images

--- main.tf
+++ main.tf
@@ -250,7 +250,7 @@ resource "aws_autoscaling_group" "app_asg" {
 
   launch_template {
     id      = aws_launch_template.docker_lt.id
-    version = "$Latest"
+    version = aws_launch_template.docker_lt.latest_version
   }
 
   instance_refresh {
@@ -258,7 +258,6 @@ resource "aws_autoscaling_group" "app_asg" {
     preferences {
       min_healthy_percentage = 50 
     }
-    triggers = ["tag"] 
   }
 
   tag {
